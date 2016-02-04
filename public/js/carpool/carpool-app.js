77da5313fdd82e3bff53ed5d6bc847e9caf771fd
Swaroop <kantheti.swaroop@gmail.com>
2016-02-04 23:29:30 +0530

    Updated code

--- public/js/carpool/carpool-app.js
+++ public/js/carpool/carpool-app.js
@@ -1,11 +1,44 @@
 
-var carpoolApp = angular.module('carpoolApp', []);
+var carpoolApp = angular.module('carpoolApp', ['ngRoute']);
+
+carpoolApp.config(function ($routeProvider) {
+    $routeProvider
+                .when('/',
+                {
+                    controller: 'MemeberInfoCtrl',
+                    templateUrl: 'home.html'
+                })
+                .when('/home',
+                {
+                    controller: 'MemeberInfoCtrl',
+                    templateUrl: 'home.html'
+                })
+                .when('/login',
+                {
+                    controller: 'MemeberInfoCtrl',
+                    templateUrl: 'login.html'
+                })
+                .when('/register',
+                {
+                    controller: 'MemeberInfoCtrl',
+                    templateUrl: 'register.html'
+                })
+                .when('/carpoolreq',
+                {
+                    controller: 'PoolReqCtrl',
+                    templateUrl: 'carpoolreq.html'
+                })
+                .otherwise({ redirectTo: '/' })
+});
 
 carpoolApp.controller('MemeberInfoCtrl', function($scope, $http) {
 
-	$scope.memberInfo = {firstName:'Dilip'};
+
+
+	//$scope.memberInfo = {firstName:'Dilip'};
 
 	$scope.postMemberInfo = function() {
+        $scope.showModal = false;
 		console.log($scope.memberInfo);
 		$http({
 			method: 'POST',
@@ -13,7 +46,12 @@ carpoolApp.controller('MemeberInfoCtrl', function($scope, $http) {
 			headers: {'Content-Type': 'application/json'},
 			data: $scope.memberInfo
 		}).then(function success(response) {
+    
+    
+        $scope.showModal = !$scope.showModal;
+    
 			console.log(response);
+            //alert('ding dong')
 		}, function error(response) {
 
 		});
@@ -40,3 +78,50 @@ carpoolApp.controller('PoolReqCtrl', function($scope, $http) {
 	};
 
 });
+
+
+
+
+
+
+carpoolApp.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
---
+++ public/js/carpool/modals.js
@@ -0,0 +1,55 @@
+
+
+carpoolApp.controller('MainCtrl', function ($scope) {
+    $scope.showModal = false;
+    $scope.showModal2  = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
+    $scope.ding = function(){
+        $scope.showModal2 = !$scope.showModal2;
+    };    
+  });
+
+carpoolApp.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
+  

92f0eaf1606ade4e83deeb16e22e1e7990780f42
Swaroop <kantheti.swaroop@gmail.com>
2016-04-27 19:10:16 +0530

    updated code

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ var users = require('./routes/users');
 var app = express();
 
 var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost:27017/carpool", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  } else {
-    console.log(err);
-  }
+MongoClient.connect("mongodb://localhost:27017/carpool", function (err, db) {
+    if (!err) {
+        console.log("We are connected");
+    } else {
+        console.log(err);
+    }
 });
 
 // view engine setup
@@ -35,10 +35,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -46,30 +46,30 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,111 @@
 var express = require('express');
 var router = express.Router();
+var assert = require('assert');
 
 var carpoolDb = undefined;
 var MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect("mongodb://localhost:27017/carpool", function(err, db) {
-  if(!err) {
-  	carpoolDb = db;
-    console.log("We are connected");
-  } else {
-    console.log(err);
-  }
+MongoClient.connect("mongodb://localhost:27017/carpool", function (err, db) {
+    if (!err) {
+        carpoolDb = db;
+        console.log("We are connected");
+    } else {
+        console.log(err);
+    }
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
+
 /* GET home page. */
-router.post('/registerUser', function(req, res, next) {
-	//console.log("inside register memebers = " + req.body);
-	try {
-		var memberInfo = req.body;
-		delete memberInfo['confirmPassword'];
-		memberInfo['createdOn'] = new Date();
-		carpoolDb.collection('members').insert(memberInfo, function(err, result) {
-			console.log(result);
-			console.log(err);
-			if(err === null) {
-	  			res.send({'status': 'success' });
-			} else {
-				console.log(err);
-				if(err.message.indexOf('email_1 dup key') > -1)
-	  			res.send({'status': 'failure' , 'statusMsg': 'User already registered with the email ' + memberInfo.email});
-			}
-		});
-	} catch (err) {
-		console.log(err);
-	}
+router.post('/registerUser', function (req, res, next) {
+    //console.log("inside register memebers = " + req.body);
+    var memberInfo = req.body;
+    console.log(memberInfo['firstName']);
+    carpoolDb.collection('members').insert(memberInfo, function (err, result) {
+        console.log(result);
+        if (result.insertedCount === 1) {
+            res.send({ 'status': 'success' });
+        } else {
+            console.log(err.message);
+            if (err.message.indexOf('email_1 dup key') > -1)
+                res.send({ 'status': 'failure', 'statusMsg': 'User already registered with the email ' + memberInfo.email });
+        }
+    });
 });
+
+
+var findUser = function (UserName, Pws, res, callback) {
+    console.log('This is find User');
+    console.log(UserName);
+    console.log(Pws);
+
+    carpoolDb.collection('members').findOne({ "email": UserName, "password": Pws }, function (err, document) {
+        if (document != null) {
+            console.log(" Successfully loggedin ");
+            console.log(document._id);
+            res.send({ 'status': 'success', 'UserID ': Result});
+        } else {
+            console.log(" FAILED ... loggedin ");
+             res.send({ 'status': 'failure' });
+        }
+    });
+
+    /*
+    var result = "false";
+    cursor.each(function (err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+    console.log(" Successfully loggedin ");
+    console.log(doc);
+    result = "true";
+    callback(result);
+    } else {
+    if (result != "true") {
+    result = "false";
+    console.log(" FAILED ... loggedin ");
+    callback(result);
+    }
+            
+    }
+    });*/
+
+};
+
+
+var findusers = function (callback) {
+    var cursor = carpoolDb.collection('members').find();
+    cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            callback();
+        }
+    });
+};
+
+router.post('/loginUser', function (req, res, next) {
+    // console.log("inside register memebers = " + req.body);
+    console.log("inside loginuser post");
+    var memberInfo = req.body;
+    console.log(memberInfo['email']);
+    console.log(memberInfo['password']);
+    //findusers(function() {  
+    //});
+    findUser(memberInfo['email'], memberInfo['password'], res, function (Result) { 
+  /*  if(Result == null)
+    {
+        res.send({ 'status': 'failure' });
+    } else{*/
+        
+    //}
+    });
+});
+
+
+
 
 module.exports = router;
